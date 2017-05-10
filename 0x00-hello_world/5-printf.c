21019d754537389c63a3fc362c338cdfd1cfcd8a
Tope <tope628@gmail.com>
2017-05-10 23:27:43 +0000

    change 0

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - main function
+* Return: 0 to print string
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
