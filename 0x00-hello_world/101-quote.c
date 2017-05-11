aee3c909dfb55bdbc69226fcfeb57255cc76f4a7
Tope <tope628@gmail.com>
2017-05-11 04:05:00 +0000

    revise again

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	const char msg[] = "and that piece of art is useful\" - Dora Korpar\
-, 2015-10-19";
+, 2015-10-19\n";
 	write(STDERR_FILENO, msg, sizeof(msg) - 1);
 
 	return (1);
