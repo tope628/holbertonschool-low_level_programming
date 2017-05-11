ea0edcbb55a1d7d313ae04bac38e79d17435200a
Tope <tope628@gmail.com>
2017-05-11 03:11:17 +0000

    commiting files and removing

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+* main - prints string
+* Return: 1 prints to standard error
+*/
+
+int main(void)
+{
+	const char msg[] = 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
+	write(STDERR_FILENO, msg, sizeof(msg) - 1 );
+
+	return(1);
+}
