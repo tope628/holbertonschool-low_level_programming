bbbfdb37d638e92cfa600b8f0d397f217620c156
Tope <tope628@gmail.com>
2017-05-12 23:09:44 +0000

    101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,27 +8,36 @@ int main(void)
 {
 	int num1 = '0';
 	int num2 = '0';
+	int num3 = '0';
 
 	while (num1 <= '9')
 	{
 		while (num2 <= '9')
 		{
-			if (num1 < num2)
+			while (num3 <= '9')
 			{
-				putchar(num1);
-				putchar(num2);
+				if ((num1 < num2) && (num2 < num3))
+				{
+					putchar(num1);
+					putchar(num2);
+					putchar(num3);
 
-				if ((num1 != '8') || (num2 != '9'))
+				if ((num1 != '7') || (num2 != '8') || (num3 != '9'))
 				{
 					putchar(44);
 					putchar(32);
 				}
 			}
-			num2++;
+			num3++;
 		}
-		num2 = '0';
-		num1++;
+		num3 = '0';
+		num2++;
+		}
+	num3 = '0';
+	num2 = '0';
+	num1++;
 	}
 	putchar('\n');
+
 	return (0);
 }
