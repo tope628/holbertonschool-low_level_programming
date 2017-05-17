4960c3c1fa7b23a1f1c70bf0a9cff00d04d9da6c
Tope <tope628@gmail.com>
2017-05-17 21:33:24 +0000

    betty is happy

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,46 @@
+#include "holberton.h"
+/**
+* print_times_table - print n times table
+* Return: void
+* @n: integer given
+*/
+void print_times_table(int n)
+{
+	int y, x, answer, tens, ones, huns;
+
+	if ((n < 15) && (n >= 0))
+	{
+	for (y = 0; y <= n; y++)
+	{
+		for (x = 0; x <= n; x++)
+		{
+			answer = (y * x);
+			if (answer <= 9)
+			{
+				if ((answer == 0) && (x == 0))
+				{
+					_putchar(answer + '0');
+				}
+				else
+				{
+					_putchar(' '); _putchar(' '); _putchar(answer + '0');
+				}
+			}
+			else if (answer < 100)
+			{
+				tens = answer / 10; ones = answer % 10;
+				_putchar(' '); _putchar(tens + '0'); _putchar(ones + '0');
+			}
+			else
+			{
+				huns = answer / 100; tens = ((answer / 10) % 10); ones = answer % 10;
+				_putchar(huns + '0'); _putchar(tens + '0'); _putchar(ones + '0');
+			}
+			if (x != n)
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+	}
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -10,3 +10,4 @@ void times_table(void);
 int print_last_digit(int);
 void print_to_98(int n);
 void jack_bauer(void);
+void print_times_table(int n);
