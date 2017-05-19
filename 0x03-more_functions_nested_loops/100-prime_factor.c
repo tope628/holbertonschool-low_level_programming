f6799c62935efe3fb95d41da0ec763c458c3e42e
Tope <tope628@gmail.com>
2017-05-19 19:26:26 +0000

    100

---
+++ 0x03-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+* main - entry point
+* Return: 0 if successful
+*/
+
+int main(void)
+{
+	long int n = 612852475143;
+	long int max, divisor;
+
+	divisor = 2;
+
+	while (n > 0)
+	{
+		if (n % divisor)
+		{
+			divisor++;
+		}
+		else
+		{
+			max = n;
+			n = n / divisor;
+
+			if  (n  == 1)
+			{
+				printf("%li", max);
+				break;
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
