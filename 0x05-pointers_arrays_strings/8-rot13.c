21f7eda1bba93bb22c0b0892ddcf977b92332b6d
Tope <tope628@gmail.com>
2017-05-26 21:46:20 +0000

    rot13

---
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+  * rot13 - encode in rot13
+  * @s: given
+  * Return: string
+  */
+
+char *rot13(char *s)
+{
+	int i, x;
+
+	char in[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char out[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i]; i++)
+	{
+		for (x = 0; in[x]; x++)
+		{ 
+			if (s[i] == in[x])
+			{
+				s[i] = out[x];
+				break;
+			}
+		}
+	}
+	return (s);
+}
