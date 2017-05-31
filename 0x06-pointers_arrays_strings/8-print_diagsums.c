06d36bc87ac385cdf072886c31e6b5146cdd665f
Tope <tope628@gmail.com>
2017-05-31 19:05:22 +0000

    matrix

--- 0x06-pointers_arrays_strings/8-print_diagsums.c
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -17,12 +17,12 @@ void print_diagsums(int *a, int size)
 	for (i = 0; i < size; i++)
 	{
 		x = (i * size) + i;
-		sum1 += *(a + x);
+		sum1 += a[x];
 	}
 	for (j = 0; j < size; j++)
 	{
 		x = (j * size) + (size - 1 - j);
-		sum2 += *(a + x);
+		sum2 += a[x];
 	}
 	printf("%i, %i\n", sum1, sum2);
 }
