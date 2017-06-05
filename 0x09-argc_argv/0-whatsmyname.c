614085f46bdb62f970e2f189eac1f1f465590d3f
Tope <tope628@gmail.com>
2017-06-05 22:55:39 +0000

    09 first commit

---
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+  *
+  *
+  */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
---
+++ 0x09-argc_argv/1-args.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+  *
+  *
+  */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+}
