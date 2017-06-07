060566cb2f47c0a9c06f0cd222e6f786ee3dae85
Tope <tope628@gmail.com>
2017-06-07 06:48:23 +0000

    points for 5 existing

---
+++ 0x0A-malloc_free/5-argstostr.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * argstostr - grid space
+ * @ac: given
+ * @av: given
+ * Return: 0
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *s;
+
+	if (ac <= 0 || av == NULL)
+	{
+		return (NULL);
+	}
+
+}
