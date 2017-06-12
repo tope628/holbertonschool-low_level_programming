4d73fd156afe6761ebeaab78556c5c61ef38c4be
Tope <tope628@gmail.com>
2017-06-12 23:28:44 +0000

    first commit

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - creating a dog
+ * @name: dog name
+ * @owner: owner of dog
+ * @age: number
+ * Description: creating a dog
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif /* _DOG_H_ */
