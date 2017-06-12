262f8722f6b8cac811bc68f8091d6bf2918521ae
Tope <tope628@gmail.com>
2017-06-12 22:40:32 +0000

    2 again

--- 0x0C-preprocessor/2-main.c
+++ 0x0C-preprocessor/2-main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-#define BLURT printf("%s\n", __FILE__)
 /**
   * main - print blurt
   * Return: voided
   */
 int main(void)
 {
-	BLURT;
+	printf("%s\n", __FILE__);
+	return (0);
 }
