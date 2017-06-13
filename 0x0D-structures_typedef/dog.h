d9a5e1e4ba77447aee3b2022651ed775a54eca54
Tope <tope628@gmail.com>
2017-06-13 00:04:37 +0000

    1 and 3

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -13,8 +13,9 @@ struct dog
 	char *owner;
 	float age;
 };
-#endif /* _DOG_H_ */
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-/* dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d); */
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif /* _DOG_H_ */
