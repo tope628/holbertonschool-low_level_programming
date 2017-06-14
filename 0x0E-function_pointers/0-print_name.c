db7ea4450338fe6e500c878261f2a7d088d648ba
Tope <tope628@gmail.com>
2017-06-14 21:59:04 +0000

    first commit

---
+++ 0x0E-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+  * print_name - prints name
+  * @name: name to print
+  * @f: function pointer
+  * Return: voided
+  */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
