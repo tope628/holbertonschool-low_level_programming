35474de870d1bc135c5b87a1a3db693495bf820d
Tope <tope628@gmail.com>
2017-06-18 18:06:13 +0000

    betty on header

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,7 +1,16 @@
 #ifndef _VARIADIC_FUNCTIONS_H_
 #define _VARIADIC_FUNCTIONS_H_
 #include <stdarg.h>
-#include <stdio.h>
+/**
+  * struct correct_format -  print specific type
+  * @per: type
+  * @f: function
+  */
+typedef struct correct_format
+{
+	char *per;
+	void (*f)(va_list);
+} cor_f;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
@@ -12,10 +21,5 @@ void print_i(va_list list);
 void print_f(va_list list);
 void print_s(va_list list);
 
-typedef struct correct_format
-{
-	char *per;
-	void (*f)();
-}cor_f;
 
 #endif /**_VARIADIC_FUNCTIONS_H_ */
