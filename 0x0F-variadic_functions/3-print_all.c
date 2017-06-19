e039e20a7e6a020ce15472c6c5a8c37a206fbb9b
Tope <tope628@gmail.com>
2017-06-19 00:00:45 +0000

    3

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -2,6 +2,48 @@
 #include <stdio.h>
 
 /**
+  * print_c - prints different formats
+  * @list: format given as a string
+  * Return: proper format printed
+  */
+void print_c(va_list list)
+{
+	printf("%c", va_arg(list, int));
+}
+/**
+  * print_all - prints different formats
+  * @format: format given as a string
+  * Return: proper format printed
+  */
+
+void print_i(va_list list)
+{
+	printf("%i", va_arg(list, int));
+}
+/**
+  * print_f - prints different formats
+  * @list: format given as a string
+  * Return: proper format printed
+  */
+
+void print_f(va_list list)
+{
+	printf("%f", va_arg(list, double));
+}
+/**
+  * print_s - prints different formats
+  * @list: format given as a string
+  * Return: proper format printed
+  */
+void print_s(va_list list)
+{
+	char *str = va_arg(list, char *);
+
+	if (str == NULL)
+		str ="(nil)";
+	printf("%s", str);
+}
+/**
   * print_all - prints different formats
   * @format: format given as a string
   * Return: proper format printed
@@ -21,39 +63,20 @@ void print_all(const char * const format, ...)
 	int i = 0, j = 0;
 	va_start(list, format);
 
-	while (format[i] && format)
+	while (format[i] != '\0' && format != '\0')
 	{
-		while (f[j].per)
+		while (f[j].per != NULL)
 		{	
 			if (f[j].per[0] == format[i])
 			{
 				f[j].f(list);
+				printf(", ");
 			}
-		}
-		if (f[j].per == NULL)
-		{
-			printf("(nil)");
-		}
 		j++;
-	}
+		}
 	i++;
 	j = 0;
+	}
 va_end(list);
 putchar('\n');
-}
-void print_c(va_list list)
-{
-	printf("%c", va_arg(list, int));
-}
-void print_i(va_list list)
-{
-	printf("%i", va_arg(list, int));
-}
-void print_f(va_list list)
-{
-	printf("%f", va_arg(list, double));
-}
-void print_s(va_list list)
-{
-	printf("%s", va_arg(list, char *));
 }
