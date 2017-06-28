62156328f0b515c0f7740168a02ddcd7deb7a113
Tope <tope628@gmail.com>
2017-06-28 23:43:07 +0000

    9

---
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+  * insert_nodeint_at_index - print list of nodes
+  * @head: pointer to list
+  * @idx: pointer to str
+  * @n: pointer to str
+  * Return: list
+  */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *list = *head;
+	listint_t *new;
+	unsigned int count = 0;
+
+	if (head == NULL)
+		return (NULL);
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	while (list && count <= idx)
+	{
+		if (count == idx - 1)
+		{
+			new->next = list->next;
+			list->next = new;
+
+			return (new);
+		}
+		count++;
+		list = list->next;
+	}
+	return (NULL);
+}
