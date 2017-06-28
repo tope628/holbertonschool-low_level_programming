00d0ea4ece3883463e43d4887916ccadb7c99e2a
Tope <tope628@gmail.com>
2017-06-28 18:04:26 +0000

    2

---
+++ 0x12-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+  * add_node - print list of nodes
+  * @head: pointer to list
+  * @str: pointer to str
+  * Return: list
+  */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *list;
+
+	list = malloc(sizeof(listint_t));
+
+	if (list == NULL)
+		return (NULL);
+
+	list->next = *head;
+	list->n = n;
+	*head = list;
+
+	return (list);
+}
