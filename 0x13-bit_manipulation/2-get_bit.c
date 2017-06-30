a73d27a721e8a8b69d54b3881bb545e35ca03cbc
Tope <tope628@gmail.com>
2017-06-30 17:40:24 +0000

    2-get_bit.c

---
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <limits.h>
+#include <stdio.h>
+/**
+  * get_bit - value of bit at given index
+  * @n: number given
+  * @index: index given
+  * Return: value of bit or -1 if error
+  */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int i = (sizeof(n) * CHAR_BIT - 1);
+	int j = index;
+	if (n == 0 && index == 0)
+		return (0);
+	while (i >= 0)
+	{
+		if (i == j)
+		{
+			return ((n >> index) & 1);
+		}
+		i--;
+	}
+	return (-1);
+}
