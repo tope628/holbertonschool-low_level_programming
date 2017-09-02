56c763c8ab1aba9b18d122892d8343c39e9c1c6f
Tope <tope628@gmail.com>
2017-09-02 05:54:22 +0000

    0

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -9,6 +9,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_hash;
+	unsigned int i;
 
 	new_hash = malloc(sizeof(hash_table_t));
 	if (new_hash == NULL)
@@ -19,6 +20,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	{
 		free(new_hash);
 		return (NULL);
+	}
+	for(i = 0; i < size; i++)
+	{
+		new_hash->array[i] = NULL;
 	}
 	return (new_hash);
 }
