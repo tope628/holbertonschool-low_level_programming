72f3d28f0b3e5df5e5a4802cc5f7b78c38a60422
Tope <tope628@gmail.com>
2017-12-02 02:16:03 +0000

    betty

--- 0x1D-search_algorithms/0-linear.c
+++ 0x1D-search_algorithms/0-linear.c
@@ -1,13 +1,16 @@
 #include "search_algos.h"
 
 /**
- * linear_search - searches for a value in an array of integers using the Linear search algorithm
+ * linear_search - searches for a value in an array
+ *  of integers using the Linear search algorithm
  * @array: array given
- * @size: size_t of array 
+ * @size: size_t of array
  * @value: target value to find
+ * Return: index of value if found; otherwise -1
  */
 
-int linear_search(int *array, size_t size, int value) {
+int linear_search(int *array, size_t size, int value)
+{
 	unsigned int i;
 
 	if (array == NULL)
